bb9196711bf826d7e36dc707f6db7b304ef77f42
dmithamo <dennisbmithamo@gmail.com>
2018-04-12 22:04:44 +0300

    Enable app reset

--- app/src/main/java/com/example/dennis/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/dennis/tictactoe/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.dennis.tictactoe;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -21,5 +22,11 @@ public class MainActivity extends AppCompatActivity {
     public void loadFiveByFive(View view) {
         setContentView(R.layout.activity_five_square);
         Toast.makeText(this, "This is as yet unhandled", Toast.LENGTH_LONG).show();
+    }
+
+    public void resetGame(View view) {
+        Intent mIntent = getIntent();
+        finish();
+        startActivity(mIntent);
     }
 }
