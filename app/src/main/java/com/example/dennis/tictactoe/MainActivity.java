d055920f0a37bd054f7ba5c935cdccbe175ebea2
dmithamo <dennisbmithamo@gmail.com>
2018-04-12 21:27:29 +0300

    Add buttons for selecting preferred game layout (3 x 3 or 5 x 5)

--- app/src/main/java/com/example/dennis/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/dennis/tictactoe/MainActivity.java
@@ -1,29 +1,25 @@
 package com.example.dennis.tictactoe;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_preload);
 
-//        Set up the Mode selector spinner
-        Spinner modeSelector = findViewById(R.id.game_mode);
-        ArrayAdapter<CharSequence> modeOptions = ArrayAdapter.createFromResource(this,
-                R.array.modes, android.R.layout.simple_spinner_item);
-        modeOptions.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        modeSelector.setAdapter(modeOptions);
+    }
 
-//        set up Difficulty selector spinner
-        Spinner difficultySelector = findViewById(R.id.game_difficulty);
-        ArrayAdapter<CharSequence> difficultyOptions = ArrayAdapter.createFromResource(this,
-                R.array.difficulty, android.R.layout.simple_spinner_item);
-        difficultyOptions.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        difficultySelector.setAdapter(difficultyOptions);
+    public void loadThreeByThree(View view) {
+        setContentView(R.layout.activity_three_square);
+    }
+
+    public void loadFiveByFive(View view) {
+        setContentView(R.layout.activity_five_square);
+        Toast.makeText(this, "This is as yet unhandled", Toast.LENGTH_LONG).show();
     }
 }
