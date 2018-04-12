493b77d15ab36e7872b2595718788842ace66631
dmithamo <dennisbmithamo@gmail.com>
2018-04-12 23:58:08 +0300

    Attempt to add click listener to boxes

--- app/src/main/java/com/example/dennis/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/dennis/tictactoe/MainActivity.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +30,18 @@ public class MainActivity extends AppCompatActivity {
         Intent mIntent = getIntent();
         finish();
         startActivity(mIntent);
+    }
+
+    public void makeMove(View view) {
+        ImageView sqr_11 = findViewById(R.id.box_11);
+        String contents = Integer.toString(sqr_11.getId());
+        if(contents == ""){
+            sqr_11.setImageResource(R.drawable.x);
+
+        }else{
+            Toast.makeText(this, "Already marked! Play somewhere else! " +
+                    contents, Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
